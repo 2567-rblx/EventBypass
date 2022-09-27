6dc5b3671c55053b2ef50ec21b9b2636bc6d981f
2567-rblx <100501186+2567-rblx@users.noreply.github.com>
2022-09-27 15:48:46 +1000

    Create Walmart Land.lua

---
+++ games/Walmart Land.lua
@@ -0,0 +1,5 @@
+while true do
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.FerrisModel9000.Base.plataformStopFerrisWheel.CFrame
+	task.wait(0.1)
+	fireproximityprompt(workspace.FerrisModel9000.Base.plataformStopFerrisWheel.ProximityPrompt)
+end
